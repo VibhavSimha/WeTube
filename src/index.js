ccb3b454e3d9dbf15ab027fb930253b068e00f72
VibhavSimha <vibhavsimha@gmail.com>
2025-04-03 23:22:02 +0530

    initial server listen-run

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello from index.js !!");
+import { config } from "dotenv";
+config({ path: "./src/.env" });
+import {app} from "./app.js"
+
+const PORT=process.env.PORT
+app.listen(PORT,()=>{
+    console.log(`Server is running on port:${PORT}`);
+    
+})
