9ba51cd127b830bad14fd96ec9246cfb02b1db99
VibhavSimha <vibhavsimha@gmail.com>
2025-04-03 23:55:35 +0530

    connect to db

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+//connect database
+
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
+
+const connectDB=async ()=>{
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\nMongoDB Connected. Host: ${connectionInstance.connection.host}`);
+        
+    }
+    catch(error){
+        console.log("MongoDB Connecton error: ",error);;
+        process.exit(1)
+        
+    }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import { config } from "dotenv";
 config({ path: "./src/.env" });
 import {app} from "./app.js"
+import connectDB from "./db/index.js";
 
-const PORT=process.env.PORT
-app.listen(PORT,()=>{
-    console.log(`Server is running on port:${PORT}`);
+const PORT=process.env.PORT || 8001
+
+connectDB().then(
+    ()=>{
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port:${PORT}`);
+            
+        })
+    }
+).catch((err)=>{
+    console.log("MongoDB connection error ",err);
     
 })
+
