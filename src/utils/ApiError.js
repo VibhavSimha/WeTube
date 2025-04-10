155ebd40d610899c3c4cc3ea0ae25f644d076622
VibhavSimha <vibhavsimha@gmail.com>
2025-04-10 22:54:57 +0530

    add apierror handler

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message="Error Occured",
+        errors=[],//Error could be multiple
+        stack="" //Location of errors
+    ){
+        super(message)//Invoke super class constructor
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success=false
+        this.errors=errors;
+
+        if(stack){
+            this.stack=stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
